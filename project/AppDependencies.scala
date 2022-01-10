92eb09944a30f0005acedadeca39d4b17a8cd65c
anjumabbas <khaakpaayali@gmail.com>
2022-01-10 12:05:41 +0000

    API-5241 - removed enumeratum dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,8 +9,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.16.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "1.26.0-play-28",
-    // "com.beachape"            %% "enumeratum-play-json"       % enumeratumVersion,
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "1.26.0-play-28"
   )
 
   val test = Seq(
