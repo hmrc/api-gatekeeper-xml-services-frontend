06d5067eb210eb4e5ad5cd35c6d89e453a1463b7
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2023-10-19 09:11:40 +0100

    APIS-6512: fix email menu to go to correct GK page

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,13 +6,13 @@ import sbt._
 object AppDependencies {
 
   lazy val enumeratumVersion = "1.6.2"
-  lazy val bootstrapVersion  = "7.21.0"
+  lazy val bootstrapVersion  = "7.22.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"   % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"           % "7.19.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"           % "7.23.0-play-28",
     "uk.gov.hmrc"       %% "internal-auth-client-play-28" % "1.6.0",
-    "uk.gov.hmrc"       %% "emailaddress"                 % "3.7.0",
+    "uk.gov.hmrc"       %% "emailaddress"                 % "3.8.0",
     "org.typelevel"     %% "cats-core"                    % "2.4.2",
     "org.apache.commons" % "commons-csv"                  % "1.8",
     "commons-io"         % "commons-io"                   % "2.11.0",
