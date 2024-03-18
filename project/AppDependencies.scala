a16521f6fe44a8a8eff180c8b5194eeee4ec848e
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2024-03-18 21:16:36 +0000

    APIS-6943 Upgrade play frontend to 9.0.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt._
 
 object AppDependencies {
 
-  lazy val bootstrapVersion  = "8.4.0"
+  lazy val bootstrapVersion  = "8.5.0"
   val apiDomainVersion       = "0.14.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"   % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"   % "8.5.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"   % "9.0.0",
     "uk.gov.hmrc"       %% "internal-auth-client-play-30" % "1.10.0",
     "uk.gov.hmrc"       %% "emailaddress"                 % "3.8.0",
     "org.typelevel"     %% "cats-core"                    % "2.9.0",
