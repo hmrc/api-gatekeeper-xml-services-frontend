231c40fa1d63c45c54b7471613bf4163a618dfb5
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2025-03-11 14:06:04 +0000

    API-8271: bump library version, address bobby warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt._
 
 object AppDependencies {
 
-  lazy val bootstrapVersion  = "9.7.0"
+  lazy val bootstrapVersion  = "9.11.0"
   val apiDomainVersion       = "0.19.1"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"   % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"   % "11.11.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"   % "11.12.0",
     "uk.gov.hmrc"       %% "internal-auth-client-play-30" % "3.0.0",
     "org.typelevel"     %% "cats-core"                    % "2.12.0",
     "org.apache.commons" % "commons-csv"                  % "1.12.0",
