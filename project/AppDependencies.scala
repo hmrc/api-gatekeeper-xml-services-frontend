164ac15c409964f72130dc6a67b7915d42eb598f
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2025-06-24 09:19:38 +0100

    APIS-7644 - Update for rebranding work (#77)
    * APIS-7644 - Update for rebranding work
    
    * APIS-7644 - PR Bot warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,13 @@ import sbt._
 
 object AppDependencies {
 
-  lazy val bootstrapVersion  = "9.11.0"
-  val apiDomainVersion       = "0.19.1"
+  lazy val bootstrapVersion = "9.13.0"
+  val apiDomainVersion      = "0.19.1"
+  val playfrontendVersion   = "12.6.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"   % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"   % "11.12.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"   % playfrontendVersion,
     "uk.gov.hmrc"       %% "internal-auth-client-play-30" % "3.1.0",
     "org.typelevel"     %% "cats-core"                    % "2.12.0",
     "org.apache.commons" % "commons-csv"                  % "1.12.0",
@@ -17,9 +18,9 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"         %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "org.mockito"         %% "mockito-scala-scalatest" % "1.17.29",
-    "org.jsoup"            % "jsoup"                   % "1.15.4"
+    "uk.gov.hmrc" %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "org.mockito" %% "mockito-scala-scalatest" % "1.17.29",
+    "org.jsoup"    % "jsoup"                   % "1.15.4"
   )
     .map(_ % "test")
 }
