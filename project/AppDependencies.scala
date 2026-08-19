41ce90399cea9f8ea171c762f5297b25c8b33f08
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2026-08-19 10:01:49 +0100

    APIS-8299 Upgrade to latest hmrc play frontend (#87)
    * APIS-8299 Upgrade to latest hmrc play frontend
    
    * APIS-8299 PR bot action

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,13 +4,13 @@ object AppDependencies {
 
   val bootstrapVersion     = "10.7.0"
   val apiDomainVersion     = "1.2.0"
-  val playfrontendVersion  = "12.32.0"
+  val playfrontendVersion  = "13.11.0"
   val mockitoScalaVersion  = "2.0.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"   % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"   % playfrontendVersion,
-    "uk.gov.hmrc"       %% "internal-auth-client-play-30" % "4.3.0",
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30" % "4.4.0",
     "org.typelevel"     %% "cats-core"                    % "2.13.0",
     "org.apache.commons" % "commons-csv"                  % "1.14.1",
     "commons-validator"  % "commons-validator"            % "1.10.1",
